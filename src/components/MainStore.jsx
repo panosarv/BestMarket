2727a22ba68b00bf2b4c5ceb38e07621cd051779
Panagiotis Arvanitis <97358977+panosarv@users.noreply.github.com>
2023-08-30 19:34:04 +0300

    Tracked Product Detail page changes

--- src/components/MainStore.jsx
+++ src/components/MainStore.jsx
@@ -11,7 +11,6 @@ import BakeryDiningIcon from '@mui/icons-material/BakeryDining';
 import KebabDiningIcon from '@mui/icons-material/KebabDining';
 import HealingIcon from '@mui/icons-material/Healing';
 import SoapIcon from '@mui/icons-material/Soap';
-import Slider from '@mui/material/Slider';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
@@ -29,45 +28,7 @@ const Item = styled(Paper)(({ theme }) => ({
     minWidth:150
   }));
 
-  const PriceRangeSlider = styled(Slider)({
-    color: '#5f816f',
-    height: 8,
-    '& .MuiSlider-track': {
-      border: 'none',
-    },
-    '& .MuiSlider-thumb': {
-    
-      height: 24,
-      width: 24,
-      backgroundColor: '#fff',
-      border: '2px solid currentColor',
-      '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-        boxShadow: 'inherit',
-      },
-      '&:before': {
-        display: 'none',
-      },
-    },
-    '& .MuiSlider-valueLabel': {
-      lineHeight: 1.2,
-      fontSize: 12,
-      background: 'unset',
-      padding: 0,
-      width: 32,
-      height: 32,
-      borderRadius: '50% 50% 50% 0',
-      backgroundColor: '#5f816f',
-      transformOrigin: 'bottom left',
-      transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-      '&:before': { display: 'none' },
-      '&.MuiSlider-valueLabelOpen': {
-        transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-      },
-      '& > *': {
-        transform: 'rotate(45deg)',
-      },
-    },
-  });
+ 
   
 
 function MainStore(){
