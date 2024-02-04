4e00ba15843e27d153ebcff57feb9a4190e591cd
Panagiotis Arvanitis <97358977+panosarv@users.noreply.github.com>
2024-02-04 21:24:00 +0200

    Finished ML recommendation

--- Backend/Python/server.py
+++ Backend/Python/server.py
@@ -1,65 +1,46 @@
 from flask import Flask, jsonify, request
-from keras.models import load_model
+
+from joblib import load
 import pandas as pd
-from itertools import combinations
 
 app = Flask(__name__)
 
 # Load the model
-model = load_model('model.h5')
+model = load('Backend\Python\model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the data from the POST request
+    print('request-->',request.data)
     data = request.get_json(force=True)
+    print('data-->',data)
 
     # Get the supermarkets, weather, and transport from the data
-    supermarkets = data['supermarkets']
-    weather = data['weather']
-    transport = data['transport']
+    
 
     # Generate all possible pairs of supermarkets
-    supermarket_pairs = list(combinations(supermarkets, 2))
 
     # Create a DataFrame with the cost and distance values for each pair, along with the weather and transport
-    df = pd.DataFrame([
-        {
-            'Distance1': pair[0]['distance'],
-            'Distance2': pair[1]['distance'],
-            'Cost1': pair[0]['cost'],
-            'Cost2': pair[1]['cost'],
-            'Weather': weather,
-            'Transport': transport,
-            'Supermarket1': pair[0]['name'],
-            'Supermarket2': pair[1]['name']
-        }
-        for pair in supermarket_pairs
-    ])
-
+    df = pd.DataFrame(data)
+    df.rename(columns={'name': 'supermarketId', 'distance': 'Distance', 'weatherCondition': 'WeatherCondition', 'meansOfTransport': 'MeansOfTransport', 'cost': 'Cost','timeOfDay':'TimeOfDay'}, inplace=True)
+    print('df-->',df)
+    X = df[[ 'Distance', 'Cost', 'MeansOfTransport','WeatherCondition','TimeOfDay']].astype('float32')
+    print('X-->',X)
+    y_pred = model.predict(X)
+    print('y_pred-->',y_pred)
+    print('xXXXX->',df['supermarketId'].tolist())
     # Make a prediction for each pair and calculate the selection scores
     # Calculate selection scores
-    scores = {}
-    for idx, row in df.iterrows():
-        pair = tuple(row[['Distance1', 'Distance2', 'Cost1', 'Cost2','Weather','Transport']])
-        prediction_probabilities = model.predict([pair])[0]
-        prediction = 1 if prediction_probabilities > 0.5 else 0
-        confidence = prediction_probabilities if prediction == 1 else 1 - prediction_probabilities
-        selected_supermarket = row['Supermarket1'] if prediction == 0 else row['Supermarket2']
-        not_selected_supermarket = row['Supermarket2'] if prediction == 0 else row['Supermarket1']
-        if selected_supermarket not in scores:
-            scores[selected_supermarket] = 0
-        if not_selected_supermarket not in scores:
-            scores[not_selected_supermarket] = 0
-
-        scores[selected_supermarket] += confidence
-        scores[not_selected_supermarket] += 1 - confidence
-
-
-    # Sort the supermarkets by their scores
-    top_supermarkets = sorted(scores.items(), key=lambda item: item[1], reverse=True)
+    supermarketIdList=df['supermarketId'].tolist()
+    scoresList=y_pred.tolist()
+    responseData=[]
+    for i in range(len(scoresList)):
+        responseData.append({'supermarketId': supermarketIdList[i], 'score': scoresList[i]})
+    
+    return jsonify(responseData)
+    
 
     # Return the supermarkets and their scores
-    return jsonify(top_supermarkets)
 
 if __name__ == "__main__":
     app.run()
