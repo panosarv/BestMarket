8848ef0dc00342d23e1563da219cc46dc0d570e1
Panagiotis Arvanitis <97358977+panosarv@users.noreply.github.com>
2024-12-27 02:32:31 +0200

    Update dbConfig.js

--- Backend/Config/dbConfig.js
+++ Backend/Config/dbConfig.js
@@ -1,10 +1,10 @@
 const config = {
-    user: 'bestmarket_user',
-    host: 'dpg-cm4ccp0cmk4c73cj2ffg-a.frankfurt-postgres.render.com',
-    database: 'bestmarket',
-    password: 'x5orZW8qHOOgQMjXM6vsnMcgufl65Vni',
-    port: 5432,
-    ssl:true,
+    user: '',
+    host: '',
+    database: '',
+    password: '',
+    port: ,
+    ssl:,
   };
 
   export default config;
